c243b806ce5651e68cb59e9672d28dd74b2bcb87
Johanni Brea <jbrea@users.noreply.github.com>
2025-09-10 14:10:31 +0200

    fix project_path

--- Demo/src/Demo.jl
+++ Demo/src/Demo.jl
@@ -3,7 +3,7 @@ module Demo
 using PrecompilePlutoCourse
 
 function __init__()
-    PrecompilePlutoCourse.configure(@__MODULE__)
+    PrecompilePlutoCourse.configure(@__MODULE__, project_path = joinpath(@__DIR__, ".."))
 end
 
 end # module
